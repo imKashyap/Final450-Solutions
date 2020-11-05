6e2a520a7719be8a38d9c16ddc3172fc1c2e72a1
imKashyap <rahulkashyap4041@gmail.com>
2020-11-05 15:43:10 +0530

    Added REVERSE ARRAY program

--- array/ReverseArray.java
+++ array/ReverseArray.java
@@ -1,10 +1,32 @@
-package final450.arrays;
+package final450.array;
+
+import java.util.Scanner;
 
 public class ReverseArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scr = new Scanner(System.in);
+		int len = scr.nextInt();
+		int[] arr = new int[len];
+		for (int i = 0; i < len; i++)
+			arr[i] = scr.nextInt();
+		display(reverseArray(arr, 0, len - 1));
+		scr.close();
+	}
 
+	static int[] reverseArray(int arr[], int start, int end) {
+		for (; start < end; start++, end--) {
+			arr[start] = arr[start] * arr[end];
+			arr[end] = arr[start] / arr[end];
+			arr[start] = arr[start] / arr[end];
+		}
+		return arr;
+	}
+
+	static void display(int[] arr) {
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
 	}
 
 }
