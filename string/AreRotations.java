48109883fe362f4348e748cbf1a650b248183258
Rahul Kashyap <rahulkashyap4041@gmail.com>
2021-03-01 16:56:30 +0530

    Check whether one string is a rotation of another

---
+++ string/AreRotations.java
@@ -0,0 +1,19 @@
+package practice.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AreRotations {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s1 = br.readLine().trim();
+		String s2 = br.readLine().trim();
+		System.out.println((s1 + s1).contains(s2));
+		br.close();
+	}
+
+}
+
--- string/ReverseString.java
+++ string/ReverseString.java
@@ -1,10 +1,32 @@
-package final450.string;
+package practice.string;
+
+import java.util.Scanner;
 
 public class ReverseString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scr = new Scanner(System.in);
+		int len = scr.nextInt();
+		char[] arr = new char[len];
+		for (int i = 0; i < len; i++)
+			arr[i] = scr.next().charAt(0);
+		reverseString(arr);
+		display(arr);
+		scr.close();
+	}
 
+	static void reverseString(char s[]) {
+		for (int i = 0, j = s.length - 1; i < s.length / 2; i++, j--) {
+			char temp = s[i];
+			s[i] = s[j];
+			s[j] = temp;
+		}
+	}
+
+	static void display(char[] arr) {
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
 	}
 
 }
