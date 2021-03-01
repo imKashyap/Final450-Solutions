29d717d21fc1989b1982a7661e6523300e43de2a
Rahul Kashyap <rahulkashyap4041@gmail.com>
2021-03-01 16:37:38 +0530

    Check whether a String is Palindrome or not

---
+++ string/Palindrome.java
@@ -0,0 +1,22 @@
+package practice.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Palindrome {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine().trim();
+		boolean pal = true;
+		for (int start = 0, end = s.length() - 1; start < end; start++, end--) {
+			if (s.charAt(start) != s.charAt(end)) {
+				pal = false;
+				break;
+			}
+		}
+		System.out.print(pal);
+		br.close();
+	}
+}
+
